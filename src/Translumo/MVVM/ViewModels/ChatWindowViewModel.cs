2cef2b214e1f3efea68a2e2d8cb18b6586fa3c08
ramjkeee <ramjkeee@gmail.com>
2025-09-13 09:41:42 +0700

    Revert "Allow translation while the settings window is open. There must have been a reason Danil added the warning, but I can’t find any justification for preventing users from translating with the settings window open."
    This reverts commit b1e9bdec501f13b0ed3647b81185ba25249983f0.

--- src/Translumo/MVVM/ViewModels/ChatWindowViewModel.cs
+++ src/Translumo/MVVM/ViewModels/ChatWindowViewModel.cs
@@ -91,7 +91,7 @@ namespace Translumo.MVVM.ViewModels
             }
             else
             {
-                StartTranslation();
+                StartTranslation(true);
             }
         }
 
@@ -116,6 +116,13 @@ namespace Translumo.MVVM.ViewModels
 
         private void HotKeysManagerOnOnceTranslateKeyPressed(object sender, EventArgs e)
         {
+            if (_dialogService.WindowIsOpened<SettingsViewModel>())
+            {
+                Model.AddChatItem(LocalizationManager.GetValue("Str.Chat.SettingsOpened"), TextTypes.Info);
+
+                return;
+            }
+
             var result = _dialogService.ShowWindowDialog<SelectionAreaWindow>(out var window);
             if (result.HasValue && result.Value)
             {
@@ -157,7 +164,7 @@ namespace Translumo.MVVM.ViewModels
             ChatWindowIsVisible = !ChatWindowIsVisible;
             if (ChatWindowIsVisible)
             {
-                StartTranslation();
+                StartTranslation(false);
             }
             else
             {
@@ -190,8 +197,18 @@ namespace Translumo.MVVM.ViewModels
             };
         }
 
-        private void StartTranslation()
+        private void StartTranslation(bool showWarning)
         {
+            if (_dialogService.WindowIsOpened<SettingsViewModel>())
+            {
+                if (showWarning)
+                {
+                    Model.AddChatItem(LocalizationManager.GetValue("Str.Chat.SettingsOpened"), TextTypes.Info);
+                }
+
+                return;
+            }
+
             Model.StartTranslation();
         }
 
