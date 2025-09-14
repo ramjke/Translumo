593744c522a786ea3b15571522ebc6d1cf6ae716
ramjkeee <ramjkeee@gmail.com>
2025-09-14 11:35:16 +0700

    Update version to 1.0.1

--- src/Translumo/AssemblyInfo.cs
+++ src/Translumo/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.1")]
 
 [assembly: AssemblyCompany("Translumo")]
 [assembly: AssemblyProduct("Translumo")]
