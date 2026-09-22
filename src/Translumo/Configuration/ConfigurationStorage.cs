83c8a01af15fa10f29452958bfbdbafb11fcd71a
Aleksei Vekhov <vehov@vehov.com>
2026-09-22 09:55:06 +0300

    Save settings automatically when they change instead of only on exit

--- src/Translumo/Configuration/ConfigurationStorage.cs
+++ src/Translumo/Configuration/ConfigurationStorage.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Xml.Serialization;
 using Microsoft.Extensions.Logging;
 using Translumo.Infrastructure.Encryption;
@@ -12,11 +15,14 @@ namespace Translumo.Configuration
     public class  ConfigurationStorage
     {
         private const string ENCRYPTION_PASSWORD = "p@wd!";
+        private const int SAVE_DELAY_MS = 700;
 
         private readonly IServiceProvider _serviceProvider;
         private readonly IEncryptionService _encryptionService;
         private readonly IList<Type> _configurationTypes;
         private readonly ILogger _logger;
+        private readonly object _saveLock = new object();
+        private readonly Timer _saveTimer;
 
         public ConfigurationStorage(IServiceProvider serviceProvider, IEncryptionService encryptionService, ILogger<ConfigurationStorage> logger)
         {
@@ -24,6 +30,7 @@ namespace Translumo.Configuration
             _serviceProvider = serviceProvider;
             _encryptionService = encryptionService;
             _configurationTypes = new List<Type>();
+            _saveTimer = new Timer(_ => SaveConfiguration(), null, Timeout.Infinite, Timeout.Infinite);
         }
 
         public void RegisterConfiguration<TConfiguration>()
@@ -73,30 +80,64 @@ namespace Translumo.Configuration
             {
                 _logger.LogError($"Unexpected error loading configuration");
             }
+
+            SubscribeToChanges(configurations);
         }
 
 
-        public void SaveConfiguration()
+        private void SubscribeToChanges(IEnumerable<object> configurations)
         {
-            List<object> configurations = _configurationTypes.Select(type => _serviceProvider.GetService(type)).ToList();
-
-            var serializer = new XmlSerializer(typeof(List<object>), _configurationTypes.ToArray());
-            var savePath = GetConfigurationPath();
-            _logger.LogTrace($"Saving configuration to '{savePath}'");
-            try
+            foreach (var configuration in configurations)
             {
-                using (MemoryStream ms = new MemoryStream())
+                Subscribe(configuration);
+                foreach (var property in configuration.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
                 {
-                    serializer.Serialize(ms, configurations);
-                    ms.Position = 0;
-
-                    byte[] encryptedConfig = _encryptionService.Encrypt(ms, ENCRYPTION_PASSWORD);
-                    File.WriteAllBytes(savePath, encryptedConfig);
+                    if (property.CanRead && property.GetIndexParameters().Length == 0)
+                    {
+                        Subscribe(property.GetValue(configuration));
+                    }
                 }
             }
-            catch (Exception ex)
+        }
+
+        private void Subscribe(object target)
+        {
+            if (target is INotifyPropertyChanged notifyingTarget)
             {
-                _logger.LogError(ex, $"Failed to save configuration to '{savePath}'");
+                notifyingTarget.PropertyChanged -= ConfigurationOnPropertyChanged;
+                notifyingTarget.PropertyChanged += ConfigurationOnPropertyChanged;
+            }
+        }
+
+        private void ConfigurationOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            _saveTimer.Change(SAVE_DELAY_MS, Timeout.Infinite);
+        }
+
+        public void SaveConfiguration()
+        {
+            lock (_saveLock)
+            {
+                List<object> configurations = _configurationTypes.Select(type => _serviceProvider.GetService(type)).ToList();
+
+                var serializer = new XmlSerializer(typeof(List<object>), _configurationTypes.ToArray());
+                var savePath = GetConfigurationPath();
+                _logger.LogTrace($"Saving configuration to '{savePath}'");
+                try
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        serializer.Serialize(ms, configurations);
+                        ms.Position = 0;
+
+                        byte[] encryptedConfig = _encryptionService.Encrypt(ms, ENCRYPTION_PASSWORD);
+                        File.WriteAllBytes(savePath, encryptedConfig);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Failed to save configuration to '{savePath}'");
+                }
             }
         }
 
