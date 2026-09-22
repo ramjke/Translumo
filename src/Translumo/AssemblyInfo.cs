87413fb36a19e0143cb524d12b2e590c931e98ce
Aleksei Vekhov <vehov@vehov.com>
2026-09-22 10:57:25 +0300

    Bump the application version to 1.1.0

--- src/Translumo/AssemblyInfo.cs
+++ src/Translumo/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: AssemblyFileVersion("1.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
-[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("1.1.0")]
 
 [assembly: AssemblyCompany("Translumo")]
 [assembly: AssemblyProduct("Translumo")]
